8c094a863da27750371e1ff7be82802d04b8e760
Simon Lindholm <simon.lindholm10@gmail.com>
2013-02-27 00:45:07 +0100

    No site-specific logging code

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
-//THIS STUFF SHOULD BE IGNORED FOR THIS PROJECT, IT'S JUST FOR LOGGING
-require_once '../../Configuration/config.php';
-if (SITE_ROOT == 'http://wproj.nada.kth.se/~purmonen/' && 
-    $_SERVER['SERVER_NAME'] != 'wproj.nada.kth.se') {
-    $query = '';
-    if (isset($_SERVER['QUERY_STRING'])) {
-        $query = $_SERVER['QUERY_STRING'];
-    }
-    header('location:' . SITE_ROOT . 'Public/MDI/index.php?' . $query);
-    exit;
-}
-require_once SERVER_ROOT . 'Database/base.php';
-require_once SERVER_ROOT . 'Database/log.php';
-$_SERVER['QUERY_STRING'] = 'MDI: ' . $_SERVER['QUERY_STRING'];
-$log = new Log_DAO();
-$log->updateLog();
-
-//THIS IS WHERE IT BEGINS
 //get page request
 
 $pages = array('about', 'report', 'prototype');
